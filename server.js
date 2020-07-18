344541aaed91a792a6dd8ae3dc0bd915870a547d
Evan Longwood <evanmlongwood@gmail.com>
2020-07-18 08:35:08 -0700

    body parser changes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function () {
   console.log("listening on 3000");
@@ -7,4 +10,8 @@ app.listen(3000, function () {
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/quotes", (req, res) => {
+  console.log(req.body);
 });

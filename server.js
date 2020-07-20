13c54e3624a34b388bfdb677432776dcb814ebca
Evan Longwood <evanmlongwood@gmail.com>
2020-07-20 13:50:55 -0700

    Mongo setup and progress

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,3 +16,17 @@ app.get("/", (req, res) => {
 app.post("/quotes", (req, res) => {
   console.log(req.body);
 });
+
+MongoClient.connect(
+  "mongodb+srv://mario:luigi@cluster0.tvzoj.mongodb.net/qoute-generator?retryWrites=true&w=majority",
+  { useUnifiedTopology: true }
+)
+  .then((client) => {
+    console.log("Connected to Database.");
+    const db = client.db("random-day-quotes");
+    app.use();
+    app.get();
+    app.post();
+    app.listen();
+  })
+  .catch((error) => console.log(error));
